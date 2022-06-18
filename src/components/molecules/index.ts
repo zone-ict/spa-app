facd4ff2c21ea1f399a4ba45540f230711718f07
Saskia Imani <imani.saskia@gmail.com>
2022-06-19 06:05:29 +0900

    fix: re-add settings page view

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -5,6 +5,7 @@ export { default as BookingListItem } from './BookingListItem.molecule';
 export { default as BottomBar } from './BottomBar.molecule';
 export { default as DatePicker } from './DatePicker.molecule';
 export { default as FieldRadio } from './FieldRadio.molecule';
+export { default as LanguageSelect } from './LanguageSelect.molecule';
 export { default as RatingWithAverage } from './RatingWithAverage.molecule';
 export { default as ReviewItem } from './ReviewItem.molecule';
 export { default as TopBar } from './TopBar.molecule';
